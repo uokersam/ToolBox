c34f9cd3892816b5693f0fa614d987f5fead3a50
uokersampoz <alucard123*>
2019-07-22 10:12:36 +0300

    Gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    autoprefixer = require('gulp-autoprefixer'),
-    notify = require('gulp-notify'),
-    csso = require('gulp-csso'),
-    cssbeautify = require('gulp-cssbeautify'),
-    pug = require('gulp-pug'),
-    terser = require('gulp-terser');
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  autoprefixer = require('gulp-autoprefixer'),
+  notify = require('gulp-notify'),
+  csso = require('gulp-csso'),
+  cssbeautify = require('gulp-cssbeautify'),
+  pug = require('gulp-pug'),
+  terser = require('gulp-terser');
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync({
     server: {
       baseDir: 'public',
@@ -19,58 +19,60 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('pug', function() {
+gulp.task('pug', function () {
   return gulp.src('app/**/*.pug').pipe(pug({
     pretty: true,
   })).pipe(gulp.dest('public'));
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src([
-    'node_modules/bulma/bulma.sass',
-    'app/scss/**/*.scss',
-  ])
-      .pipe(sass({outputStyle: 'expanded'}).on('error', notify.onError()))
-      .pipe(rename({
-        suffix: '.min',
-        prefix: '',
-      }))
-      .pipe(autoprefixer(['last 4 versions']))
-      .pipe(csso({
-        comments: false,
-      }))
-      .pipe(cssbeautify({
-        indent: '  ',
-        openbrace: 'separate-line',
-        autosemicolon: true,
-      }))
-      .pipe(gulp.dest('public/css'))
-      .pipe(browserSync.stream());
+      'node_modules/bulma/bulma.sass',
+      'app/scss/**/*.scss',
+    ])
+    .pipe(sass({outputStyle: 'expanded'}).on('error', notify.onError()))
+    .pipe(rename({
+      suffix: '.min',
+      prefix: '',
+    }))
+    .pipe(autoprefixer(['last 4 versions']))
+    .pipe(csso({
+      comments: false,
+    }))
+    .pipe(cssbeautify({
+      indent: '  ',
+      openbrace: 'separate-line',
+      autosemicolon: true,
+    }))
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp.src([
-    'app/js/helpers.js',
-    'app/js/common.js',
-  ])
-      .pipe(concat('scripts.min.js'))
-      .pipe(terser())
-      .pipe(gulp.dest('public/js'))
-      .pipe(browserSync.reload({stream: true}));
+      'app/js/helpers.js',
+      'app/js/common.js',
+    ])
+    .pipe(concat('scripts.min.js'))
+    .pipe(terser())
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('code', function() {
+gulp.task('code', function () {
   return gulp.src('public/**/*.html')
-      .pipe(browserSync.reload({stream: true}));
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('app/scss/**/*.scss',
-      gulp.parallel('styles'));
+    gulp.parallel('styles'));
   gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
   gulp.watch('app/**/*.pug', gulp.parallel('code', 'pug'));
   gulp.watch('app/service/**/*.css', gulp.parallel('code', 'pug'));
 });
 
 gulp.task('default',
-    gulp.parallel('styles', 'scripts', 'pug', 'browser-sync', 'watch'));
+  gulp.parallel('styles', 'scripts', 'pug', 'browser-sync', 'watch'));
+
+gulp.task('build', gulp.parallel('styles', 'scripts', 'pug'));
